bbf255ed7fedb5bd014e0822a9967c2934271710
A205PC03 <trangsitailoi@gmail.com>
2024-07-17 10:26:59 +0700

    add files

---
+++ MergeSort/MergeSort/Source.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+using namespace std;
+
+void merge(int A[], int left, int mid, int right) {
+	int n1 = mid - left + 1;  // độ dài nửa dãy đầu của A
+	int n2 = right - mid;     // độ dài nửa dãy sau của A
+
+	int* L = new int[n1];
+	int* R = new int[n2];
+
+
+	for (int i = 0; i < n1; i++)
+		L[i] = A[left + i];
+	for (int j = 0; j < n2; j++)
+		R[j] = A[mid + 1 + j];
+
+
+	int i = 0, j = 0;
+
+	int k = left;
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			A[k] = L[i];
+			i++;
+		}
+		else {
+			A[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	while (i < n1) {
+		A[k] = L[i];
+		i++;
+		k++;
+	}
+
+
+	while (j < n2) {
+		A[k] = R[j];
+		j++;
+		k++;
+	}
+
+
+	delete[] L;
+	delete[] R;
+}
+
+void merge_sort(int A[], int left, int right) {
+	if (left < right) {
+		int mid = left + (right - left) / 2;
+
+		
+		merge_sort(A, left, mid);
+		merge_sort(A, mid + 1, right);
+
+		
+		merge(A, left, mid, right);
+	}
+}
+
+int main() {
+	int A[] = { 12, 11, 13, 5, 6, 7 };
+	int n = sizeof(A) / sizeof(A[0]);
+
+	cout << "Mang ban dau: ";
+	for (int i = 0; i < n; i++)
+		cout << A[i] << " ";
+	cout << endl;
+
+	merge_sort(A, 0, n - 1);
+
+	cout << "Mang da sap xep: ";
+	for (int i = 0; i < n; i++)
+		cout << A[i] << " ";
+	cout << endl;
+
+	system("pause");  // Tạm dừng chương trình trước khi kết thúc
+	return 0;
+}
---
+++ TimKiemNhiPhan/TimKiemNhiPhan/TimKiemNhiPhan/Source.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int binary_search(int A[], int n, int key) {
+	int left = 0;            // vị trí phần tử đầu tiên trong mảng
+	int right = n - 1;       // vị trí phần tử cuối cùng trong mảng
+
+	while (left <= right) {
+		int mid = left + (right - left) / 2;  // vị trí giữa mảng
+
+		if (A[mid] == key) {
+			return mid;  // tìm thấy key tại vị trí mid
+		}
+		else if (key < A[mid]) {
+			right = mid - 1;  // tìm kiếm trong nửa trái của mảng
+		}
+		else {
+			left = mid + 1;  // tìm kiếm trong nửa phải của mảng
+		}
+	}
+
+	return -1;  // không tìm thấy key trong mảng
+}
+
+int main() {
+	int A[] = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+	int n = sizeof(A) / sizeof(A[0]);
+	int key = 7;
+
+	int result = binary_search(A, n, key);
+	if (result != -1) {
+		cout << "Phan tu " << key << " duoc tim thay tai vi tri " << result << "." << endl;
+	}
+	else {
+		cout << "Phan tu " << key << " khong co trong mang." << endl;
+	}
+
+	system("pause");  // Tạm dừng chương trình trước khi kết thúc
+	return 0;
+}
